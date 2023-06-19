263978f431e022b71a0a43b349bea1108802bae8
Sulaymon Yahyo 🌙 <sulaymonyahyo1w@gmail.com>
2023-06-19 17:15:09 +0500

    first commit

---
+++ src/test/java/com/suyo/quran/QuranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyo.quran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
