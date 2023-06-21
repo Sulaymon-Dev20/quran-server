49f4b039a0358eb4049fd41d1255794647e1b522
Sulaymon Yahyo 🌙 <sulaymonyahyo1w@gmail.com>
2023-06-21 12:06:47 +0500

    Juz controller 4/1

---
+++ src/main/java/com/suyo/quran/controller/JuzController.java
@@ -0,0 +1,19 @@
+package com.suyo.quran.controller;
+
+import com.suyo.quran.service.JuzService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/juz")
+@Validated
+public class JuzController {
+    final JuzService juzService;
+
+    public JuzController(JuzService juzService) {
+        this.juzService = juzService;
+    }
+
+
+}
