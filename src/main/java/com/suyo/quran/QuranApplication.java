88cb10aaed6497a08fa136a9f67f07256ad9b682
MacBookPro <sulaymon1w@gmail.com>
2023-07-02 20:49:29 +0500

    🗑️removed useless code

--- src/main/java/com/suyo/quran/QuranApplication.java
+++ src/main/java/com/suyo/quran/QuranApplication.java
@@ -3,11 +3,10 @@ package com.suyo.quran;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 public class QuranApplication {
 
