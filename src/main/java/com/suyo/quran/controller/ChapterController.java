7eb253149b92d8e5d9194c6f676660bc5497594f
MacBookPro <sulaymon1w@gmail.com>
2023-07-04 10:17:36 +0500

    🔧 code review

--- src/main/java/com/suyo/quran/controller/ChapterController.java
+++ src/main/java/com/suyo/quran/controller/ChapterController.java
@@ -4,10 +4,7 @@ import com.suyo.quran.models.Language;
 import com.suyo.quran.models.Response;
 import com.suyo.quran.service.ChapterService;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -24,18 +21,18 @@ public class ChapterController {
         this.chapterService = chapterService;
     }
 
-    @GetMapping("/{language}")
-    public Response getChapterListByLanguage(@PathVariable Language language) {
+    @GetMapping
+    public Response getChapterList(@RequestParam(defaultValue = "DEFAULT") Language language) {
         return chapterService.getChapterListByLanguage(language);
     }
 
-    @GetMapping("/all/{language}")
-    public Response getChapterSource(@PathVariable Language language) {
+    @GetMapping("/all")
+    public Response getChapterSource(@RequestParam(defaultValue = "DEFAULT") Language language) {
         return chapterService.getChapterByLanguage(language);
     }
 
     @GetMapping("/{language}/{number}")
-    public Response getChapterByLanguage(@NotNull @PathVariable Language language, @PathVariable("number") @Min(1) @Max(114) Integer chapterNumber) {
+    public Response getChapter(@NotNull @PathVariable Language language, @PathVariable("number") @Min(1) @Max(114) Integer chapterNumber) {
         return chapterService.getChapterByLanguage(language, chapterNumber);
     }
 }
