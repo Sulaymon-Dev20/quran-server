fcff87347d86fca1403adcd92204b4c7d778dd02
MacBookPro <sulaymon1w@gmail.com>
2023-07-10 00:54:53 +0500

    ⚙️ Controller path changed

--- src/main/java/com/suyo/quran/controller/JuzController.java
+++ src/main/java/com/suyo/quran/controller/JuzController.java
@@ -27,7 +27,7 @@ public class JuzController {
         this.juzService = juzService;
     }
 
-    @GetMapping()
+    @GetMapping
     @ApiOperation(value = "View a list of available products", notes = "Lorem ```Ipsum``` is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "model ``` asdf asdf``` <br/> "),
