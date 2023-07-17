a671f70a22de1dc63861a330e0ddcf813ebdc99b
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-07-17 21:34:56 +0500

    🫧 small changes

--- src/main/java/com/suyo/quran/entities/enums/ThemeEnum.java
+++ src/main/java/com/suyo/quran/entities/enums/ThemeEnum.java
@@ -1,8 +1,5 @@
 package com.suyo.quran.entities.enums;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(title = "Language Model", name = "Language Model", description = "Language all")
 public enum ThemeEnum {
     DARK,
     LIGHT,
