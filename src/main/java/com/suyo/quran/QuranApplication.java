704ebc5c3eaf4095123e0f71dd7c2b3e96bbe80b
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-08-15 17:30:43 +0500

    🕸️ Spring cloud netflix eureka client 3/1

--- src/main/java/com/suyo/quran/QuranApplication.java
+++ src/main/java/com/suyo/quran/QuranApplication.java
@@ -2,6 +2,7 @@ package com.suyo.quran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -15,8 +16,10 @@ import org.springframework.context.annotation.PropertySources;
     @PropertySource("classpath:properties/application-security.properties"),
     @PropertySource("classpath:properties/application-swagger.properties"),
     @PropertySource("classpath:properties/application-telegram.properties"),
+    @PropertySource("classpath:properties/application-cloud.properties"),
 })
 @ComponentScan(basePackages = {"com.suyo.quran", "org.telegram.telegrambots"})
+@EnableDiscoveryClient
 public class QuranApplication {
 
     public static void main(String[] args) {
