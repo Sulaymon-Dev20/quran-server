f6aff7aa9fb5c0c4bbbbff19e8c78944ccf214e2
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-08-18 15:20:56 +0500

    👮‍ Security bug fixed and cloud and some properties

--- src/main/java/com/suyo/quran/config/SecurityConfiguration.java
+++ src/main/java/com/suyo/quran/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,19 +25,13 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf()
-            .disable()
-            .authorizeHttpRequests()
-            .requestMatchers(
-                "/api/user/**", "/api/quiz/**"
-            )
-            .authenticated()
-            .anyRequest()
-            .permitAll()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/user/**", "/api/quiz/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll())
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         log.info("👮‍️ Spring security started Boss 🫡");
--- src/main/java/com/suyo/quran/entities/AbsNameEntity.java
+++ src/main/java/com/suyo/quran/entities/AbsNameEntity.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbsNameEntity {
     @Id
+    @Column(columnDefinition = "uuid default gen_random_uuid()")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
