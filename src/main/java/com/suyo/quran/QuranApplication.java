84f0a028c246f7e1187ffd1124ceb7df2da4c821
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-08-18 15:19:46 +0500

    🪚 all telegram integration removed

--- src/main/java/com/suyo/quran/QuranApplication.java
+++ src/main/java/com/suyo/quran/QuranApplication.java
@@ -3,7 +3,6 @@ package com.suyo.quran;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
@@ -15,10 +14,8 @@ import org.springframework.context.annotation.PropertySources;
     @PropertySource("classpath:properties/application-jpa.properties"),
     @PropertySource("classpath:properties/application-security.properties"),
     @PropertySource("classpath:properties/application-swagger.properties"),
-    @PropertySource("classpath:properties/application-telegram.properties"),
     @PropertySource("classpath:properties/application-cloud.properties"),
 })
-@ComponentScan(basePackages = {"com.suyo.quran", "org.telegram.telegrambots"})
 @EnableDiscoveryClient
 public class QuranApplication {
 
